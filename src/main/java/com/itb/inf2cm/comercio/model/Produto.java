55db0cb614a7dafff85178eeab9be5ddd7f5af6d
Yasmin <rm86193@estudante.fieb.edu.br>
2023-06-20 09:05:42 -0300

    Criação do set get

--- src/main/java/com/itb/inf2cm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2cm/comercio/model/Produto.java
@@ -5,11 +5,58 @@ public class Produto {
 	private Long id;
 	private String nome;
 	private String codigoDeBarras;
+	private String descricao;
 	private double preco;
 	
 	// public : Acesso liberado para todas as classes
 	// private: Acesso liberado apenas para os membros da própria
 	//          classe, ou seja, só é acessado dentro da classe.
 	
+	//Métodos Setter´s e Getter´s : Utilizamos para atribuir e recuperar informações
+	//de um referido atributo "RESPECTIVAMENTE". Para cada atributo teremos o set e o get.
 	
+	public void setID(Long id) {
+		this.id = id;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCodigoDeBarras() {
+		return codigoDeBarras;
+	}
+
+	public void setCodigoDeBarras(String codigoDeBarras) {
+		this.codigoDeBarras = codigoDeBarras;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 }
